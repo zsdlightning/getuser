60d48766699302925b2b3681f928aa853729ab3a
Jefferson Heard <jeff@renci.org>
2014-03-22 22:18:24 +0000

    solve crossdomain.xml problem

--- views.py
+++ views.py
@@ -2,6 +2,9 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 # Create your views here.
+def crossdomain(request):
+    return render(request, 'crossdomain/crossdomain.xml',{})
+
 def index(request):
     if request.user.is_authenticated():
 	return HttpResponse(request.user.username)
